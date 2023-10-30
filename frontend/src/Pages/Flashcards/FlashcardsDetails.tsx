9682fc31e61637cffd066ab7a8055976c1013f39
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-10-30 23:32:15 +0100

    add delete flashcard set func

--- frontend/src/Pages/Flashcards/FlashcardsDetails.tsx
+++ frontend/src/Pages/Flashcards/FlashcardsDetails.tsx
@@ -1,10 +1,13 @@
 import { motion, useIsPresent } from "framer-motion";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Button from "../../Components/Button.tsx";
 import getFlashcardSet from "../../lib/flashcards/getFlashcardSet.ts";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import { t } from "i18next";
+import getUserObject from "../../lib/getUser.ts";
+import toast from "react-hot-toast";
+import { BsFillTrashFill, BsFillPencilFill, BsFillPlayFill } from "react-icons/bs";
 
 interface user {
   id: number,
@@ -56,6 +59,32 @@ export default function FlashcardsDetails() {
   });
 
   const { id } = useParams();
+  const loggedUser: user = getUserObject();
+
+  const navigate = useNavigate();
+
+  const deleteSet = async () => {
+
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/flashcard/set/${id}`, {
+      method: "DELETE",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    if (!response.ok) {
+      return toast.error(t("somethingWentWrong"));
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    toast.success(t("flashcardDeleted"));
+    setTimeout(() => {
+      return navigate("/flashcards");
+    }, 750);
+  };
 
   useEffect(() => {
     if (id) {
@@ -69,17 +98,25 @@ export default function FlashcardsDetails() {
     <div className="flex-1 text-white py-6 w-[80%] max-w-[1300px] mx-auto">
       <div className="flex justify-between items-center w-full">
         <h2 className="text-3xl py-4">{title}</h2>
-        <Button isLink={true} to={`/flashcards/learn/${id}`} className="text-lg px-4 py-2 mt-8" width="w-42"
-                type="default">{t('startLearning')}</Button>
+        <div className="flex gap-3">
+          {loggedUser.id === user.id &&
+            <Button className="text-lg px-4 py-2 mt-8" width="w-42"
+                    type="alt" onClick={deleteSet}><BsFillTrashFill /></Button>}
+          {loggedUser.id === user.id &&
+            <Button className="text-lg px-4 py-2 mt-8" width="w-42"
+                    type="alt"><BsFillPencilFill /></Button>}
+          <Button isLink={true} to={`/flashcards/learn/${id}`} className="text-lg px-4 py-2 mt-8" width="w-42"
+                  type="default">{t("startLearning")} <BsFillPlayFill /></Button>
+        </div>
       </div>
-      <p>{t('author')}: <Link to={`/profile/${user.id}`}>{user.username}</Link></p>
-      {forkedFrom && <p>{t('forkedFrom')}: <Link to={`/profile/${forkedFrom.id}`}>{forkedFrom.username}</Link></p>}
-      <p>{t('createdAt')}: {formatDistanceToNow(createdAt ? new Date(createdAt) : new Date(), { addSuffix: true }) || ""}</p>
-      <p>{t('lastModified')}: {formatDistanceToNow(updatedAt ? new Date(updatedAt) : new Date(), { addSuffix: true }) || ""}</p>
-      <p>{t('description')}: {description}</p>
-      <p className="text-gray-400">{t('publicity')}: {publicity}</p>
+      <p>{t("author")}: <Link to={`/profile/${user.id}`}>{user.username}</Link></p>
+      {forkedFrom && <p>{t("forkedFrom")}: <Link to={`/profile/${forkedFrom.id}`}>{forkedFrom.username}</Link></p>}
+      <p>{t("createdAt")}: {formatDistanceToNow(createdAt ? new Date(createdAt) : new Date(), { addSuffix: true }) || ""}</p>
+      <p>{t("lastModified")}: {formatDistanceToNow(updatedAt ? new Date(updatedAt) : new Date(), { addSuffix: true }) || ""}</p>
+      <p>{t("description")}: {description}</p>
+      <p className="text-gray-400">{t("publicity")}: {publicity}</p>
       <div className="flex flex-col gap-4 mt-8">
-        <p>{t('questionsAndAnswers')}:</p>
+        <p>{t("questionsAndAnswers")}:</p>
         {flashCards.map(flashCard => {
           return (
             <div key={flashCard.id} className="flex gap-2 items-center">
